89f88c7c4d3268c7b6faeb1a1260626fa8ab02d6
ZHUXUYIN <67779333+USERZHUXUYIN@users.noreply.github.com>
2020-11-04 16:28:15 -0600

    Add files via upload

---
+++ main.java
@@ -0,0 +1,110 @@
+package subway;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class main {
+
+	static int Over=404;
+	static int i;
+	static Map<String, Integer> stationflag=new HashMap<String, Integer>();//存放站点和站点的标记
+	static Map<String, String> stationline=new HashMap<String,String>();//存放站点和地铁线名称
+
+public static void main(String[] args) {
+		int n=400;
+		double Map[]=new double[n*n];
+		double Ma[]=Map;
+		for (int i=0;i<n*n;i++)
+			Map[i] = Over;
+		String name[]=new String[n];
+		String nam[]=name;
+		int path[]=new int[400];
+		int pa[] = path;
+		List list =Arrays.asList(name);
+		int x=0;
+		try {
+			//读取文件
+			String filePath="C:\\Users\\18418\\Desktop\\地铁线路信息.txt";
+			File file=new File(filePath);
+			InputStreamReader read=new InputStreamReader(new FileInputStream(file),"UTF-8"); 
+            BufferedReader br=new BufferedReader(read);
+            String lineTxt=null;
+		while ((lineTxt=br.readLine())!=null) {
+			String na=null;
+			String[] arr=lineTxt.split(" ");//取出语句的空格后存放到'arr'数组
+			//取出地铁线名字并且删除
+			String linename=arr[0];
+			for(int i=0;i<arr.length-1;i++)
+				arr[i]=arr[i+1];
+			for (String namer : arr){						
+				if (stationflag.keySet().contains(namer)) { //map包含的键的set视图中是否包含已经包含了该站点
+					int temp=0;
+					for(int i=0;i<arr.length;i++) {
+						if(namer.equals(arr[i])) 
+							temp++;
+						}
+					if(temp==1){
+						stationflag.put(namer,(stationflag.get(namer)+1));
+						stationline.put(namer,stationline.get(namer)+","+linename);
+					}
+				}
+				else {//否则新存入一个站点信息
+					name[x]=namer;
+					x++;
+					stationflag.put(namer,1);
+					stationline.put(namer,linename);
+				}
+				if(na!=null) {
+					Map[n*list.indexOf(namer)+list.indexOf(na)]=2;
+					Map[n*list.indexOf(na)+list.indexOf(namer)]=2;	
+				}
+				na=namer;
+			}
+		}
+		br.close();
+		}catch (Exception e) {
+			System.out.println("找不到指定的文件");
+		}
+		
+		for (i=0;i<n;i++)
+			Map[n*i+i] = 0;
+		String start,dest;
+		method e=new method();
+		Scanner scanf=new Scanner(System.in);
+		
+		System.out.print("请输入起点站名:");
+		start = scanf.nextLine();
+		while (!e.exist(name, start, n)) {
+			System.out.println("站点输入错误！");
+			System.out.print("请重新输入起点站名:");
+			start = scanf.nextLine();
+		}
+		System.out.print("请输入终点站名:");
+		dest = scanf.nextLine();
+		while (!e.exist(name, dest, n)) {
+			System.out.println("站点输入错误！");
+			System.out.print("请重新输入终点站名:");
+			dest = scanf.nextLine();
+		}
+		
+		if(e.exist(name,dest,n)) {
+			method f=new method();
+			float distan=(float)f.Dijkstra(Ma,pa,n,f.exam(nam,start,n), f.exam(nam,dest,n));//调用迪杰斯特拉算法查出此结点到其他点最短路径
+			int s, d;
+			s=f.exam(nam,start,n);
+			d=f.exam(nam,dest,n);
+			System.out.print("最佳乘地铁路线：");
+			f.print(pa,nam,s,d);
+			System.out.print("——>"+dest);
+		}
+		return;
+	}
+
+}
---
+++ method.java
@@ -0,0 +1,67 @@
+package subway;
+
+public class method {
+	static int Over = 404;
+	//迪杰斯特拉算法实现
+	public double Dijkstra(double Map[],int path[],int n,int m,int d) {
+		int u,t;
+		double min,result;
+		double dist[]=new double[n+1];
+		int s[]=new int[n+1];
+		for(int i=0;i<n;i++){
+			dist[i]=Map[n*m+i];
+			s[i]=0;
+			if(i!=m&&dist[i]<Over){
+				path[i]=m;
+			}
+			else
+				path[i]=-1;
+		}
+		s[m]=1;
+		for(int i=0;i<n-1;i++){
+			min=Over;
+			u=m;
+			for(int j=0;j<n;j++){
+				if (s[j]==0&&dist[j]<min){
+					u=j;
+					min=dist[j];
+				}
+			}
+			s[u]=1;
+			for(t=0;t<n;t++){
+				if (s[t]==0&&dist[u]+Map[n*u+t]<dist[t]) {
+					dist[t]=(float)(dist[u]+Map[n*u+t]);
+					path[t]=u;
+				}
+			}
+		}
+		result=dist[d];
+		return result;
+	}
+	public int exam(String name[],String nam,int n) {
+		int i=0;
+		while (i<n&&!name[i].equals(nam)) {
+			i++;
+		}
+		return i;
+	}
+	//输出站点
+	public void print(int pa[],String nam[],int d,int i)
+	{
+		if (pa[i]>=0) {
+			print(pa,nam,d,pa[i]);
+			System.out.print("——>" +nam[pa[i]]);	
+		}
+	}
+	//判断是否是其中的节点
+	public static boolean exist(String name[],String nam,int n) {
+		int i=0;
+		while (i<n) {
+			if (name[i]!=null&&name[i].equals(nam)) {
+				return true;
+			}
+			i++;
+		}
+		return false;
+	} 
+}
